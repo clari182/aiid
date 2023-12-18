1943138caf1331970a61e0fccccfbf2564cf5e78
Clara Youdale <clara@botsfactory.io>
2023-12-18 10:48:26 -0300

    Remove react-helmets tags

--- site/gatsby-site/gatsby-ssr.js
+++ site/gatsby-site/gatsby-ssr.js
@@ -3,7 +3,35 @@ import './src/global.css';
 import 'bootstrap-daterangepicker/daterangepicker.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import './src/custom.css';
+import { cloneDeepWith } from 'lodash';
 
 import { wrapRootElement, wrapPageElement } from './gatsby-shared';
 
-export { wrapPageElement, wrapRootElement };
+// cleanup html before render
+const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+
+  replaceHeadComponents(omitDeep(headComponents, ['data-react-helmet']));
+};
+
+/**
+ * remove properties from collection deep
+ * @param collection
+ * @param excludeKeys
+ * @returns {any}
+ */
+const omitDeep = (collection, excludeKeys) =>
+  cloneDeepWith(collection, (value) => {
+    console.log('HOLA');
+    if (value && typeof value === 'object') {
+      for (const key of excludeKeys) {
+        try {
+          delete value[key];
+        } catch (_) {
+          // console.log("ignore", _);
+        }
+      }
+    }
+  });
+
+export { wrapPageElement, wrapRootElement, onPreRenderHTML };
