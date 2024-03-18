1e93c54409b7a25c7ce21e329c7cd564c0f81291
Clara Youdale <clara@botsfactory.io>
2024-03-18 14:29:24 -0300

    Enable "Should render the TSNE visualization"

--- site/gatsby-site/cypress/e2e/integration/tsneVisualization.cy.js
+++ site/gatsby-site/cypress/e2e/integration/tsneVisualization.cy.js
@@ -14,7 +14,7 @@ const styleObject = (styleString) => {
 describe('TSNE Visualization', () => {
   const url = '/summaries/spatial';
 
-  it.skip('Should render the TSNE visualization', () => {
+  it('Should render the TSNE visualization', () => {
     cy.visit(url);
     cy.get('[data-cy="tsne-visualization"] [data-cy="tsne-plotpoint"]').should('exist');
   });
